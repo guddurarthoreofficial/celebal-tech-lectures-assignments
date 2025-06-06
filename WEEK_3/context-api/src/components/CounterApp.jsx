9347906934ae5851432718b1ce08d9cb230f68f6
Guddu Kumar <guddu748081@gmail.com>
2025-06-06 18:39:51 +0530

    project all bsic routes is done and basic static pages is done

--- WEEK_3/context-api/src/components/CounterApp.jsx
+++ WEEK_3/context-api/src/components/CounterApp.jsx
@@ -14,3 +14,5 @@ const CounterApp = () => {
 }
 
 export default CounterApp
+
+
--- WEEK_3/context-api/src/context/CounterContextProvider.jsx
+++ WEEK_3/context-api/src/context/CounterContextProvider.jsx
@@ -2,10 +2,11 @@ import React, { createContext, useState } from 'react';
 
 export const CounterContext = createContext();
 
+
 const CounterContextProvider = ({ children }) => {
   const [count, setCount] = useState(0);
 
-  
+
   return (
     <CounterContext.Provider value={{ count, setCount }}>
       {children}
